e9c4574e83272e5e5279888cb29ba5dfd7bbc0b1
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-09-28 17:15:05 -0400

    added tests and migrated db

--- test/system/funs_test.rb
+++ test/system/funs_test.rb
@@ -1,9 +1,15 @@
 require "application_system_test_case"
 
 class FunsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit funs_url
-  #
-  #   assert_selector "h1", text: "Fun"
-  # end
+  test "visiting the index" do
+    visit funs_path
+
+    assert_selector "h1", text: "Fun"
+  end
+
+  test "adding a fun itm" do
+    visit funs_path
+
+    click_on "New Fun"
+  end
 end
